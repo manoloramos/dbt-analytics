c5952bd33cb87d92918b011936a7c477ecdf41a8
M. Ramos <mramos.dev+dbt@pm.me>
2025-05-08 17:22:51 +0000

    products

--- models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
@@ -1,22 +1,6 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
-
 WITH src_products AS (
     SELECT * 
     FROM {{ source('sql_server_dbo', 'PRODUCTS') }}
-
-    UNION ALL
-
-    SELECT
-       'no product' AS product_id,
-       0.0 AS price,
-       'no product' AS name,
-       0 AS inventory,
-       NULL AS date_delete,
-       CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP) AS date_load
     ),
 
 products_output AS (
@@ -28,6 +12,6 @@ products_output AS (
         _fivetran_deleted::BOOLEAN AS is_deleted,
         CONVERT_TIMEZONE('UTC', _fivetran_synced::TIMESTAMP) AS date_loaded
     FROM src_products
-    )
+)
 
 SELECT * FROM products_output
