659b4771a40b0e513ba299dd759218a92b40c995
M. Ramos <mramos.dev+dbt@pm.me>
2025-05-16 17:57:12 +0000

    save

--- models/staging/sql_server_dbo/stg_sql_server_dbo__promos_statuses.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promos_statuses.sql
@@ -2,4 +2,12 @@ WITH promo_statuses AS (
     SELECT
         status
     FROM {{ ref('base_sql_server_dbo__promos_statuses') }}
+),
+
+promos_statuses_output AS (
+    SELECT 
+        *
+    FROM promo_statuses
 )
+
+SELECT * FROM promos_statuses_output
